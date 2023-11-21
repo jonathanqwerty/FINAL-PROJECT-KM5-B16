60a4725972de33ae7010cb95ba559b3f7e333755
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-11-21 13:08:11 +0700

    fix bug

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,8 @@ const express = require("express"),
   validate = require("../middlewares/validate"),
   schema = require("../validatorSchema/authValidatorSchema");
 
-router.post(
-  "/register",
-  validate(schema.registerValidator),
-  controller.register
-);
-router.post("/login", validate(schema.loginValidator), controller.login);
+router.post("/register", controller.register);
+router.post("/login", controller.login);
 router.post("/reset-password", controller.resetPassword);
 router.post("/set-password", controller.setPassword);
 
