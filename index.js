6da459c7492df968fc8886fed522311c564d9d54
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-11-21 16:34:16 +0700

    cors

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require("express"),
   app = express(),
   PORT = process.env.PORT || 3000,
-  // cors = require("cors"),
+  cors = require("cors"),
   router = require("./routes/index");
 
 require("dotenv").config();
 
 app.use(express.json({ strict: false }));
-// app.use(cors());
+app.use(cors());
 app.use("/api/v1", router);
 
 // Handle 404 route
 app.get("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   return res.status(404).json({
     error: "berhasi, tetapi endpoint tidak ada",
   });
