4af02508e6376d90b919b4871f4bd3557729c21c
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-11-22 15:26:12 +0700

    remove function

--- routes/auth.js
+++ routes/auth.js
@@ -8,28 +8,6 @@ const express = require("express"),
 router.post("/register", controller.register);
 router.post("/login", validate(schema.loginValidator), controller.login);
 router.post("/reset-password", controller.resetPassword);
-
-router.get("/set-password/:key", async (req, res) => {
-  try {
-    const findData = await users.findFirst({
-      where: {
-        resetPasswordToken: req.params.key,
-      },
-    });
-    if (!findData) {
-      return res.status(404).json({
-        error: "Your email is not registered in our system",
-      });
-    }
-
-    return res.json("set-password", { user: findData });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      error,
-    });
-  }
-});
 router.post("/set-password/:key", controller.setPassword);
 
 module.exports = router;
