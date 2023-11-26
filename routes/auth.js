695450fe8f4b5600d740248c50d3a89ef32188fb
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-11-26 15:05:56 +0700

    tweaks: add validasi otp

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,11 @@ router.post(
   validate(schema.registerValidator),
   controller.register
 );
-router.post("/otp/:key", controller.verifyUser);
+router.post(
+  "/otp/:key",
+  validate(schema.verifyValidator),
+  controller.verifyUser
+);
 router.post("/login", validate(schema.loginValidator), controller.login);
 router.post("/reset-password", controller.resetPassword);
 router.post("/set-password/:key", controller.setPassword);
