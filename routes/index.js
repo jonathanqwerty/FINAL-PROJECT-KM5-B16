91892bcab877129ba3e661dc4cf6000186f940cf
RezaHans <102926853+Rezahans@users.noreply.github.com>
2023-11-29 00:09:40 +0700

    Rezahans (#18)
    * Staging (#10)
    
    * test (#1)
    
    * new otp
    
    * Jonathan (#7)
    
    * test
    
    * fitur: otp
    
    * Jonathan (#8)
    
    * test
    
    * fitur: otp
    
    * Jonathan (#9)
    
    * test
    
    * fitur: otp
    
    * tweaks: add validasi otp
    
    * tweaks: add tampilan email
    
    ---------
    
    Co-authored-by: wcancur <61812726+jonathanqwerty@users.noreply.github.com>
    
    * notifications
    
    ---------
    
    Co-authored-by: wcancur <61812726+jonathanqwerty@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express"),
   router = express.Router(),
   authRouter = require("./auth"),
-  profileRouter = require("./profile");
+  profileRouter = require("./profile"),
+  notifRouter = require ("./notif");
 
 router.use("/auth", authRouter);
 router.use("/profile", profileRouter);
+router.use("/me", notifRouter);
 
 module.exports = router;
