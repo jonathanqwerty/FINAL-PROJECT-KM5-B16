adb8e643b74119d033e46ee56f50df65c880891b
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-11-28 13:25:35 +0700

    fix: register validation

--- validatorSchema/authValidatorSchema.js
+++ validatorSchema/authValidatorSchema.js
@@ -3,7 +3,14 @@ const { body } = require("express-validator");
 const registerValidator = [
   body("email").notEmpty().isEmail(),
   body("phone").notEmpty().isMobilePhone(),
-  body("password").notEmpty().isStrongPassword(),
+  body("password").notEmpty().isStrongPassword({
+    minLength: 8,
+    minUppercase: 1,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    returnScore: false,
+  }),
 ];
 
 const verifyValidator = [body("validasi").notEmpty()];
