43daf0b5b6d1826143d3f013cc29282785aa0a41
jhonitay <jhonitay06@gmail.com>
2023-11-29 20:34:05 +0700

    -

--- models/index.js
+++ models/index.js
@@ -5,4 +5,9 @@ const prisma = new PrismaClient();
 module.exports = {
   users: prisma.users,
   profiles: prisma.profiles,
+  categories : prisma.categories,
+  courses: prisma.courses,
+  reviews: prisma.reviews,
+  orders : prisma.orders,
+  myCourse : prisma.myCourse
 };
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const express = require("express"),
   router = express.Router(),
   authRouter = require("./auth"),
   profileRouter = require("./profile");
+  homeRouter = require("./home");
 
 router.use("/auth", authRouter);
 router.use("/profile", profileRouter);
+router.use("/home", homeRouter);
+
 
 module.exports = router;
