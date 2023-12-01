4d9a48fd694f609aabb90f97427e04bc96c595cb
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-12-01 20:11:19 +0700

    test

--- models/index.js
+++ models/index.js
@@ -6,13 +6,4 @@ module.exports = {
   users: prisma.users,
   profiles: prisma.profiles,
   notifications: prisma.notifications,
-<<<<<<< HEAD
-  prisma,
-  categories : prisma.categories,
-  courses: prisma.courses,
-  reviews: prisma.reviews,
-  orders : prisma.orders,
-  myCourse : prisma.myCourse
-=======
->>>>>>> 3d5a167dc8199e8a8c4190ebb6d5f48f5a92bea3
 };
