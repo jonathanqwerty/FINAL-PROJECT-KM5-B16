39c3df7525ed6dcd75b02c84279d11b2a1a99a73
wcancur <61812726+jonathanqwerty@users.noreply.github.com>
2023-12-01 20:45:29 +0700

    Jonathan (#29)
    * test
    
    * fitur: otp
    
    * tweaks: add validasi otp
    
    * tweaks: add tampilan email
    
    * feature: Oauth
    
    * feat: add Google login
    
    * tweaks: oauth uri
    
    * fix: register validation
    
    * feature: profile
    
    * tweaks: update register
    
    * fix: show otp
    
    * fix bug
    
    * profile
    
    * test conflig
    
    * update
    
    * update: update profile
    
    * data seeder
    
    * test
    
    * bug
    
    * fix: typo reviews
    
    * fix: trouble merger

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,12 @@ const express = require("express"),
   router = express.Router(),
   authRouter = require("./auth"),
   profileRouter = require("./profile"),
-<<<<<<< HEAD
-  notifRouter = require ("./notif"),
+  notifRouter = require("./notif"),
   homeRouter = require("./home"),
-  courseRouter = require("./course")
+  courseRouter = require("./course");
 router.use("/auth", authRouter);
-router.use("/profile", profileRouter);
-router.use("/me", notifRouter);
 router.use("/home", homeRouter);
 router.use("/course", courseRouter);
-=======
-  notifRouter = require("./notif");
-
-router.use("/auth", authRouter);
-router.use("/me", notifRouter,profileRouter);
-router.use("/auth", authRouter);
->>>>>>> 3d5a167dc8199e8a8c4190ebb6d5f48f5a92bea3
-
+router.use("/me", notifRouter, profileRouter);
 
 module.exports = router;
