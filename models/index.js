861489206ca3f0c72605a8895e99f652b38b31a3
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-12-01 20:13:10 +0700

    bug

--- models/index.js
+++ models/index.js
@@ -6,4 +6,9 @@ module.exports = {
   users: prisma.users,
   profiles: prisma.profiles,
   notifications: prisma.notifications,
+  categories: prisma.categories,
+  courses: prisma.courses,
+  reviews: prisma.reviews,
+  orders: prisma.orders,
+  myCourse: prisma.myCourse,
 };
