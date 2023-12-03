7525c1884f4ed2fa4fe44f89e7bd87cdad0e5f9c
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-12-03 19:48:08 +0700

    admin category

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,19 @@ const express = require("express"),
   controller = require("../controllers/adminController"),
   validate = require("../middlewares/validate"),
   schema = require("../validatorSchema/authValidatorSchema"),
-  adminToken = require('../middlewares/adminToken');
+  adminToken = require("../middlewares/adminToken");
 
-  router.post("/login", validate(schema.loginValidator), controller.login);
-  router.get("/active-user", adminToken, controller.activeUser );
-  router.get("/active-class", adminToken, controller.activeClass );
-  router.get("/active-premium", adminToken, controller.activePremium );
-  router.get("/dashboard", adminToken, controller.dashboardData );
-  router.get("/kelola-kelas", adminToken, controller.kelolaKelas );
+router.post("/login", validate(schema.loginValidator), controller.login);
+router.get("/active-user", adminToken, controller.activeUser);
+router.get("/active-class", adminToken, controller.activeClass);
+router.get("/active-premium", adminToken, controller.activePremium);
+router.get("/dashboard", adminToken, controller.dashboardData);
+router.get("/kelola-kelas", adminToken, controller.kelolaKelas);
+// course
+router.get("/course", controller.listCourse);
+router.post("/course/create/:key", controller.createCourse);
+// category
+router.get("/category", controller.listCategory);
+router.post("/category/create", controller.createCategory);
 
-  module.exports = router;
+module.exports = router;
--- routes/courseAdmin.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"),
-  router = express.Router(),
-  controller = require("../controllers/courseAdminController");
-
-router.get("/course", controller.listCourse);
-router.post("/course/create/:key", controller.course);
-
-module.exports = router;
