6b04ca5d08a7353ac77c2c026d1e1d60f75b2f4f
wcancur <61812726+jonathanqwerty@users.noreply.github.com>
2023-12-04 19:21:47 +0700

    Jonathan (#34)
    * test
    
    * fitur: otp
    
    * tweaks: add validasi otp
    
    * tweaks: add tampilan email
    
    * feature: Oauth
    
    * feat: add Google login
    
    * tweaks: oauth uri
    
    * fix: register validation
    
    * feature: profile
    
    * tweaks: update register
    
    * fix: show otp
    
    * fix bug
    
    * profile
    
    * test conflig
    
    * update
    
    * update: update profile
    
    * data seeder
    
    * test
    
    * bug
    
    * fix: typo reviews
    
    * fix: trouble merger
    
    * update: update password in profile
    
    * Fitur: Admin login, count userActive,class,premium
    
    * API dashboard dan kelola-kelas
    
    * admin
    
    * admin category
    
    * admin page
    
    * feature: CRUD category & course
    
    ---------
    
    Co-authored-by: Rezahans <rezacrent.1@gmail.com>

--- controllers/courseControllers.js
+++ controllers/courseControllers.js
@@ -1,61 +1,62 @@
 const { categories, reviews, courses, myCourse } = require("../models");
-const {Course} = require('../utils/course')
+const { Course } = require("../utils/course");
 
 module.exports = {
-    FilterCourse : async(req, res)=>{
-      // filtering sebuah course
-        try {
-        const kategori = req.query.kategori;
-        const search = req.query.search;
-        const filter = req.query.filter;
-        const level = req.query.level;
-        const type = req.body.type;
-        let user = res.user ? res.user.id : null
+  FilterCourse: async (req, res) => {
+    // filtering sebuah course
+    try {
+      const kategori = req.query.kategori;
+      const search = req.query.search;
+      const filter = req.query.filter;
+      const level = req.query.level;
+      const type = req.body.type;
+      let user = res.user ? res.user.id : null;
 
-        const page = parseInt(req.query.page) || 1;
-        const itemsPerPage = 12;
-        const skip = (page - 1) * itemsPerPage;
+      const page = parseInt(req.query.page) || 1;
+      const itemsPerPage = 12;
+      const skip = (page - 1) * itemsPerPage;
 
+      // filter
+      let data = await courses.findMany({
+        skip,
+        take: itemsPerPage,
+        where: {
+          categories: kategori !== null ? { name: kategori } : {},
+          title: search !== null ? { contains: search } : {},
+          level: level !== null ? level : {},
+          price: type == null ? {} : type == "premium" ? { not: 0 } : 0,
+        },
+        include: {
+          categories: true,
+        },
+      });
 
-        // filter
-        let data = await courses.findMany({
-          skip,
-          take : itemsPerPage,
-          where: {
-            categories: kategori !== null ? { name: kategori } : {},
-            title : search !== null ? {contains: search}:{},
-            level : level !== null ? level:{},
-            price : type == null ? {} : type == 'premium' ? {not : 0}: 0
-          },
-          include: {
-            categories: true,
-          },
-          
-        });
+      // data = data.slice((page-1)*itemsPerPage,(page*itemsPerPage))
 
-        // data = data.slice((page-1)*itemsPerPage,(page*itemsPerPage))
+      if (data.length == 0) {
+        return res.status(404).json({ message: "not found data" });
+      }
+      console.log("jumlah data : " + data.length);
 
-        if(data.length == 0){return res.status(404).json({message : "not found data"})}
-        console.log('jumlah data : '+ data.length)
+      // mendapatkan count popularity dan memperbaiki return
+      let course = await Course(data, user);
 
-        // mendapatkan count popularity dan memperbaiki return
-        let course = await Course(data,user)
+      // filter type course
+      filter !== null
+        ? filter == "paling baru"
+          ? course.sort((a, b) => a.rilis - b.rilis)
+          : course.sort((a, b) => b.orders - a.orders)
+        : console.log("no filter");
 
-        // filter type course
-        filter !== null ? 
-        filter == 'paling baru' ? 
-        course.sort((a, b) => a.rilis - b.rilis) : 
-        course.sort((a, b) => b.orders - a.orders) : console.log('no filter')
-
-        return res.status(200).json({
-            course
-        })
-        } catch (error) {
-          console.log(error);
-          return res.status(500).json({
-            error,
-            message: "Internal server error",
-          });
-        }
+      return res.status(200).json({
+        course,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        error,
+        message: "Internal server error",
+      });
     }
-}
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const express = require("express"),
   notifRouter = require("./notif"),
   homeRouter = require("./home"),
   courseRouter = require("./course"),
-  historyRouter = require('./history');
+  historyRouter = require("./history"),
+  adminRouter = require("./admin");
 router.use("/auth", authRouter);
 router.use("/home", homeRouter);
 router.use("/course", courseRouter);
 router.use("/me", notifRouter, profileRouter, historyRouter);
+router.use("/admin", adminRouter);
 
 module.exports = router;
